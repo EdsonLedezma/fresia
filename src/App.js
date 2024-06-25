d5852a3aacd52b4812325d9cb120259ab09bd2f5
EdsonLedezma <edsonmon985@gmail.com>
2024-06-24 21:46:45 -0600

    carrousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Estilo general de tu aplicación
+import ImageGallery from 'react-image-gallery'; // Importa el componente de la librería
+
+// Importa los estilos CSS de react-image-gallery (según la documentación)
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+// Define las imágenes que mostrará la galería
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageGallery items={images} />
     </div>
   );
 }
